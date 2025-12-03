779e70017b6ebeb97c8975488550a8e756e37a7d
Casey Allred <sbditto85@gmail.com>
2025-12-02 18:28:25 -0700

    Day 2 part 1

--- src/Day02/Solution.hs
+++ src/Day02/Solution.hs
@@ -22,14 +22,72 @@ import qualified Data.Text.Read as TextRead
 
 parseInput :: Text -> [(Int, Int)]
 parseInput input =
-  error "do this"
+  input
+    & Text.strip
+    & Text.splitOn ","
+    & fmap (Text.breakOn "-")
+    & (fmap . fmap) (Text.drop 1)
+    & fmap (\(start, end) ->
+             (fst . fromRight' . TextRead.decimal $ start, fst . fromRight' . TextRead.decimal $ end)
+           )
+
+  where
+    fromRight' =
+      Either.fromRight (error "Could not parse input")
 
 input :: Text
 input = TextEncoding.decodeUtf8Lenient $(FileEmbed.embedFileRelative "src/Day02/input.txt")
 
 phaseOne :: Text -> Int
 phaseOne input =
-  error "do this"
+  let
+    ranges =
+      parseInput input
+
+    invalidNumbers = do
+      range <- ranges
+      findInvalid (fst range) (snd range)
+  in
+    sum invalidNumbers
+
+findInvalid :: Int -> Int -> [Int]
+findInvalid start end =
+  find start end []
+  where
+
+    find :: Int -> Int -> [Int] -> [Int]
+    find start end soFar =
+      if start > end then
+        soFar
+      else
+        let
+          soFar' =
+            if isInvalid start then
+              start : soFar
+            else
+              soFar
+        in
+          find (start + 1) end soFar'
+
+      where
+        isInvalid number =
+          let
+            text =
+              Text.show number
+
+            textLength =
+              Text.length text
+
+            middle =
+              textLength `div` 2
+
+            (firstHalf, secondHalf) =
+              Text.splitAt middle text
+          in
+            if textLength `mod` 2 /= 0 then
+              False
+            else
+              firstHalf == secondHalf
 
 phaseTwo :: Text -> Int
 phaseTwo input =
@@ -38,8 +96,8 @@ phaseTwo input =
 day02 :: IO ()
 day02 = do
   putStrLn "Day02 Phase 1"
-  let someVariable = phaseOne input --
-  putStrLn $ "Some Variable: " ++ tshow someVariable
+  let someVariable = phaseOne input -- 13919717792
+  putStrLn $ "Invalid numbers: " ++ tshow someVariable
 
   putStrLn "Day02 Phase 2"
   let someOtherVariable = phaseTwo input --
